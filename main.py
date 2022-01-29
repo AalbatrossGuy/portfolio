fdc6721c9675728601e139f444bde60a95c1ddae
0AaalbatrosS GuY <thexcelsiorisback@gmail.com>
2022-01-29 06:52:42 +0530

    Server-side stuff

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
-@app.route('/page=<number>')
-def page(number=None):
-    return f"you are on page {number}!"
+@app.route('/about')
+def aboutme():
+    return render_template('about.html')
+
